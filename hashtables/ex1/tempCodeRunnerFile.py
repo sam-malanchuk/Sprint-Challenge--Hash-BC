0cd21ee6d06d9830906570b2edff9630ca27e063
Sam Malanchuk <malanchuksam@gmail.com>
2020-04-18 16:16:21 -0400

    completed hash tables ex2 and passed tests

--- hashtables/ex1/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
-# answer_4 = get_indices_of_item_weights(weights_4, 9, 7)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,30 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # insert each ticket into the hash table by source airport
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    # array to hold the route results
+    route_array = []
+
+    # hold the search location (starts with NONE)
+    search = "NONE"
+    # get the destination for the search location
+    ticket = hash_table_retrieve(hashtable, search)
+    # check while the destination of the search location is not NONE (final destination)
+    while ticket != "NONE":
+        # add the next destination to the route array
+        ticket = hash_table_retrieve(hashtable, search)
+        # if we reach NONE then return the array
+        if ticket == "NONE":
+            return route_array
+        # if we have a valid location, add and update the search term
+        else:
+            route_array.append(ticket)
+        search = ticket
+
+# Understand: Get an array of n amount of ticket objects. Each ticket object has
+# a source and destination airport. The location of departure has a source of NONE
+# and the final destination has a destination of NONE. Need to add the tickets to a
+# hash table and then reconstruct the trip starting with None and ending at None. 
